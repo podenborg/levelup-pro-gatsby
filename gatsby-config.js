e0be5ca50a5c102c3f0cb706395409bcd94c49b5
Patrick Odenborg <patrick.odenborg@pearson.com>
2020-09-14 20:35:13 -0600

    trying agin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
+        name: `assets`,
         path: `${__dirname}/static/assets`,
       },
     },
